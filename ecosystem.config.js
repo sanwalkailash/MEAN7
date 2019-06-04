36c527d7a6c1d12996d75907010d04e41c27a9ca
sanwalkailash <sanwalkailash@gmail.com>
2019-06-04 09:18:26 +0530

    updated pm2 eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'API',
+    name: 'Remitance',
     script: 'server.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
@@ -13,8 +13,41 @@ module.exports = {
       NODE_ENV: 'development'
     },
     env_production: {
-      NODE_ENV: 'production',
-      ENV: 'production'
+      NODE_ENV: 'production'
+    }
+  },
+  {
+    name: 'imhere',
+    script: 'server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: "3", // number of cluster use max to use all cores.
+    autorestart: true,
+    watch: true, // The watch and restart mode watches the current directory to detect file changes and auto-start.
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  },
+  {
+    name: 'storybooks',
+    script: 'server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: "3", // number of cluster use max to use all cores.
+    autorestart: true,
+    watch: true, // The watch and restart mode watches the current directory to detect file changes and auto-start.
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
     }
   }],
 
@@ -23,7 +56,7 @@ module.exports = {
       user : 'node',
       host : '212.83.163.1',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
+      repo : 'https://github.com/sanwalkailash/MEAN7.git',
       path : '/var/www/production',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
