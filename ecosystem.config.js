2ca9230c700867fe4698beebd82f25bc22d75efe
sanwalkailash <sanwalkailash@gmail.com>
2019-06-04 09:28:14 +0530

    updated pm2 eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
-    instances: "3", // number of cluster use max to use all cores.
+    instances: "2", // number of cluster use max to use all cores.
     autorestart: true,
     watch: true, // The watch and restart mode watches the current directory to detect file changes and auto-start.
     max_memory_restart: '1G',
