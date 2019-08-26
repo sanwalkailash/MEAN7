755ddf2b6cc67eeb7febcad16439a7a23528e40c
kai <kai@admins-MBP-2.lan>
2019-08-27 02:01:12 +0530

    wip

--- models/models.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (connection) {
-
-    return {
-        userSchema: require('./User')(connection),
-        tokenSchema: require('./Token')(connection),
-        accountSchema: require('./Accounts')(connection),
-    }
-}
